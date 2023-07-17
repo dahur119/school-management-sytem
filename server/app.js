13c374b3084d4a6f1466ba344684d230be61fe08
dayo <dahurdayo@example.com>
2023-07-17 10:28:19 +0100

    Merge remote change

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, "..", "public", "build")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
+//
 
 app.use("/api", apiRouter);
 
