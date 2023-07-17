902e39e9db16ed43f09aa8d36576ba75c3316aec
dayo <dahurdayo@example.com>
2023-07-17 10:37:06 +0100

    Merge remote change

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "..", "public", "build")));
+// app.use(express.static(path.join(__dirname, "..", "public", "build")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
